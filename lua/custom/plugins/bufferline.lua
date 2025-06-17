66f845797962e498bc163068b1cf8fbaf438f76f
Jackie Tsien <jackie10673311@gmail.com>
2025-06-17 16:10:58 +0800

    update alpha

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -27,11 +27,7 @@ return {
           delay = 200,
           reveal = { 'close' },
         },
-        diagnostics = 'nvim_lsp',
-        diagnostics_indicator = function(count, level)
-          local icon = level:match 'error' and ' ' or ' '
-          return ' ' .. icon .. count
-        end,
+        always_show_bufferline = true,
       },
     }
   end,
