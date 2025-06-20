af4874adbc3a42d00954e9ab6ba1958c49606589
Jackie Tsien <jackie10673311@gmail.com>
2025-06-20 15:07:46 +0800

    add flash and update toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -26,6 +26,36 @@ return {
         },
       },
     }
+    local Terminal = require('toggleterm.terminal').Terminal
+
+    -- 计算窗口尺寸和位置
+    local term_width = math.floor(vim.o.columns * 0.9)
+    local term_height = math.floor(vim.o.lines * 0.4)
+
+    local bottom_term = Terminal:new {
+      direction = 'float', -- 关键：设置为浮动类型
+      hidden = true, -- 方便在后台运行任务
+      float_opts = {
+        border = 'rounded',
+        -- 以下是定位的关键
+        width = term_width,
+        height = term_height,
+        -- 计算 x, y 坐标使其在底部居中
+        col = math.floor((vim.o.columns - term_width) / 2),
+        row = math.floor(vim.o.lines - term_height - 2), -- -2 是为了离底部边缘有一点距离
+      },
+      on_open = function(term)
+        -- 进入终端时禁用自动换行，保持排版整洁
+        vim.cmd 'setlocal nonumber norelativenumber nowrap'
+      end,
+    }
+
+    function _G.bottom_term_toggle()
+      bottom_term:toggle()
+    end
+
+    -- 为这个新的底部终端设置一个专属快捷键，例如 <leader>tb (t for terminal, b for bottom)
+    vim.api.nvim_set_keymap('n', '<leader>tf', '<cmd>lua bottom_term_toggle()<cr>', { noremap = true, silent = true, desc = 'ToggleTerm: Bottom Float' })
 
     -- --- 终端模式下的快捷键 ---
     -- 这些快捷键只在终端 buffer 中生效，所以放在 config 里是合适的
