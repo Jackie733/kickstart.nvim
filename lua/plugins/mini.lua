ecd669a87977b9c176d0eca0678bcf18b7bcbf8c
Jackie Tsien <jackie10673311@gmail.com>
2025-12-26 11:12:20 +0800

    feat: enhance mini.nvim configuration with custom commentstring support and dependencies

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,7 +1,34 @@
 return {
-  'echasnovski/mini.nvim',
-  config = function()
-    require('mini.ai').setup { n_lines = 500 }
-    require('mini.surround').setup()
-  end,
+  {
+    'echasnovski/mini.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    },
+    config = function()
+      -- Better Around/Inside textobjects
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      require('mini.surround').setup()
+
+      -- Simple and easy comment: gcc, gc{motion}, gc (visual)
+      require('mini.comment').setup {
+        options = {
+          custom_commentstring = function()
+            local cs = require('ts_context_commentstring.internal').calculate_commentstring()
+            if cs and cs ~= '' then
+              return cs
+            end
+            -- Fallback to buffer's commentstring or default
+            local bo_cs = vim.bo.commentstring
+            if bo_cs and bo_cs ~= '' then
+              return bo_cs
+            end
+            return '// %s'
+          end,
+        },
+      }
+    end,
+  },
 }
