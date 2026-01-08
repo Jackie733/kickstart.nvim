a387cb12333435559c4dea19fb1296e4cf79d51b
Jackie Tsien <jackie10673311@gmail.com>
2026-01-08 17:04:38 +0800

    chore: remove redundant git-blame plugin and clean up options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -13,109 +13,94 @@ vim.g.have_nerd_font = false
 --  For more options, you can see `:help option-list`
 
 -- Make line numbers default
-vim.o.number = true
+vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
+vim.opt.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
+vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+  vim.opt.clipboard = 'unnamedplus'
 end)
 
 -- Enable break indent
-vim.o.breakindent = true
+vim.opt.breakindent = true
 
 -- 自动缩进设置
-vim.o.autoindent = true    -- 新行继承当前行的缩进
-vim.o.smartindent = true   -- 智能缩进（根据语法自动调整）
-vim.o.expandtab = true     -- 用空格代替 Tab
-vim.o.shiftwidth = 2       -- 缩进宽度
-vim.o.tabstop = 2          -- Tab 显示宽度
-vim.o.softtabstop = 2      -- 编辑时 Tab 宽度
+vim.opt.autoindent = true -- 新行继承当前行的缩进
+vim.opt.smartindent = true -- 智能缩进（根据语法自动调整）
+vim.opt.expandtab = true -- 用空格代替 Tab
+vim.opt.shiftwidth = 2 -- 缩进宽度
+vim.opt.tabstop = 2 -- Tab 显示宽度
+vim.opt.softtabstop = 2 -- 编辑时 Tab 宽度
+vim.opt.shiftround = true -- Round indent
 
 -- Save undo history
-vim.o.undofile = true
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true -- Don't ignore case with capitals
 
 -- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
+-- Always show the signcolumn, otherwise it would shift the text each time
+vim.opt.signcolumn = 'yes'
 
 -- Decrease update time
-vim.o.updatetime = 250
+-- Save swap file and trigger CursorHold
+vim.opt.updatetime = 200
 
 -- Decrease mapped sequence wait time
-vim.o.timeoutlen = 300
+vim.opt.timeoutlen = 300
 
 -- Configure how new splits should be opened
-vim.o.splitright = true
-vim.o.splitbelow = true
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitkeep = 'screen'
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
---
---  Notice listchars is set using `vim.opt` instead of `vim.o`.
---  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---   See `:help lua-options`
---   and `:help lua-options-guide`
-vim.o.list = true
+vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 vim.opt.laststatus = 3
 
 -- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
+vim.opt.inccommand = 'nosplit' -- preview incremental substitute
 
 -- Show which line your cursor is on
-vim.o.cursorline = false
+vim.opt.cursorline = false
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
+vim.opt.scrolloff = 4 -- Lines of context
+vim.opt.sidescrolloff = 8 -- Columns of context
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
-vim.o.confirm = true
+vim.opt.confirm = true
 
 -- LazyVim-inspired defaults
 vim.opt.autowrite = true -- Enable auto write
-vim.opt.confirm = true -- Confirm to save changes before exiting modified buffer
-vim.opt.cursorline = false -- Disable highlighting of the current line
-vim.opt.grepformat = "%f:%l:%c:%m"
-vim.opt.grepprg = "rg --vimgrep"
-vim.opt.inccommand = "nosplit" -- preview incremental substitute
-vim.opt.list = true -- Show some invisible characters (tabs...
-vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.scrolloff = 4 -- Lines of context
-vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
-vim.opt.shiftround = true -- Round indent
-vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
-vim.opt.showmode = false -- Dont show mode since we have a statusline
-vim.opt.sidescrolloff = 8 -- Columns of context
-vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-vim.opt.smartcase = true -- Don't ignore case with capitals
-vim.opt.spelllang = { "en" }
-vim.opt.splitbelow = true -- Put new windows below current
-vim.opt.splitkeep = "screen"
-vim.opt.splitright = true -- Put new windows right of current
+vim.opt.grepformat = '%f:%l:%c:%m'
+vim.opt.grepprg = 'rg --vimgrep'
+vim.opt.sessionoptions = { 'buffers', 'curdir', 'tabpages', 'winsize', 'help', 'globals', 'skiprtp', 'folds' }
+vim.opt.shortmess:append { W = true, I = true, c = true, C = true }
+vim.opt.spelllang = { 'en' }
 vim.opt.termguicolors = true -- True color support
-vim.opt.timeoutlen = 300
-vim.opt.undolevels = 10000
-vim.opt.updatetime = 200 -- Save swap file and trigger CursorHold
-vim.opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
-vim.opt.wildmode = "longest:full,full" -- Command-line completion mode
+vim.opt.virtualedit = 'block' -- Allow cursor to move where there is no text in visual block mode
+vim.opt.wildmode = 'longest:full,full' -- Command-line completion mode
 vim.opt.winminwidth = 5 -- Minimum window width
 vim.opt.wrap = false -- Disable line wrap
+
